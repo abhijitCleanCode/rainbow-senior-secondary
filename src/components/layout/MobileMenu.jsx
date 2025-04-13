5cf0cbfd39140b59557ecd4ce10be46d6e228449
Abhijit Singh <152074904+abhijitCleanCode@users.noreply.github.com>
2025-04-13 08:52:34 +0530

    Update MobileMenu.jsx

--- src/components/layout/MobileMenu.jsx
+++ src/components/layout/MobileMenu.jsx
@@ -3,6 +3,7 @@ import { X, Phone, Mail } from "lucide-react";
 import { Transition } from "@headlessui/react";
 import MobileNavItem from "../ui/MobileNavItem";
 import MobileAccordion from "../ui/MobileAccordion";
+import { Link } from "react-router-dom";
 
 const MobileMenu = ({ isOpen, setIsOpen }) => {
   const navLinks = [
@@ -92,6 +93,15 @@ const MobileMenu = ({ isOpen, setIsOpen }) => {
                 )}
               </li>
             ))}
+            <li>
+              <Link
+                to="https://rain-bow-school-beta.vercel.app"
+                className="px-3 py-2 text-gray-800 hover:text-blue-800 font-medium relative group transition-colors duration-200"
+              >
+                Login
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-800 group-hover:w-full transition-all duration-300"></span>
+              </Link>
+            </li>
           </ul>
 
           <div className="mt-8 border-t border-gray-200 pt-4">
