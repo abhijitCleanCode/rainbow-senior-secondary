8d9ffee5b80f4ac83b24faf67b0ca1bcc6bcb00b
Abhijit Singh <152074904+abhijitCleanCode@users.noreply.github.com>
2025-04-16 08:13:06 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         ))}
         <li>
           <Link
-            to="https://rain-bow-school-beta.vercel.app"
+            to="https://rain-bow-school-service-beta.vercel.app"
             className="px-3 py-2 text-gray-800 hover:text-blue-800 font-medium relative group transition-colors duration-200"
           >
             Login
