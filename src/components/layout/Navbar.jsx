17802f4f0b9aef4598bd5d6f723dc96b6288f525
Abhijit Singh <152074904+abhijitCleanCode@users.noreply.github.com>
2025-05-21 10:52:32 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -46,10 +46,18 @@ const Navbar = () => {
         ))}
         <li>
           <Link
-            to="https://rain-bow-school-service-beta.vercel.app"
+            to="http://rainbowschool.webholicinfotech.com/site/login"
             className="px-3 py-2 text-gray-800 hover:text-blue-800 font-medium relative group transition-colors duration-200"
           >
-            Login
+            Admin
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-800 group-hover:w-full transition-all duration-300"></span>
+          </Link>
+        </li>
+          <Link
+            to="http://rainbowschool.webholicinfotech.com/site/userlogin"
+            className="px-3 py-2 text-gray-800 hover:text-blue-800 font-medium relative group transition-colors duration-200"
+          >
+            User
             <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-800 group-hover:w-full transition-all duration-300"></span>
           </Link>
         </li>
